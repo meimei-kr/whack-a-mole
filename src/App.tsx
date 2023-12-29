2066860ad2957e4fb8b3fc87ce8846e0ca716749
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2023-12-29 16:54:35 +0900

    update: モグラの種類に応じて点数が変わるよう修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { useState, useEffect } from 'react'
 import hole from './assets/hole.png'
 import mole from './assets/mole.png'
 import hammer from './assets/hammer.png'
+import redMole from './assets/rainbow-mole.png'
+import blueMole from './assets/blue-mole.png'
 
-const SUCCESS_SCORE = 20
+const SUCCESS_SCORE = 10
 
 const App = () => {
   const [moles, setMoles] = useState<{ isVisible: boolean, moleType: number }[]>(
     [...Array(9)].fill({ isVisible: false, moleType: 0 })
-  )
+  ) // moleType: 0 -> 普通のモグラ, 1 -> 青モグラ, 2 -> 虹モグラ
   const [score, setScore] = useState<number>(0)
   const [isPlaying, setIsPlaying] = useState<boolean>(false)
   const [remainingTime, setRemainingTime] = useState<number>(60)
@@ -56,10 +58,22 @@ const App = () => {
     }
   }, [isPlaying, remainingTime])
 
+  const getMoleTyoe = () => {
+    const random = Math.random()
+    if (random < 0.1) {
+      return 2 // 10% の確率で虹モグラ
+    } else if (random < 0.3) {
+      return 1 // 20% の確率で青モグラ
+    } else {
+      return 0 // 70% の確率で普通のモグラ
+    }
+  }
+
   const setMoleVisibility = (index: number, isVisible: boolean) => {
     setMoles((prevMoles) => {
       const newMoles = [...prevMoles]
-      newMoles[index] = isVisible
+      const moleType = isVisible ? getMoleTyoe() : 0
+      newMoles[index] = { isVisible, moleType }
       return newMoles
     })
   }
@@ -67,7 +81,19 @@ const App = () => {
   const handleClick = (index: number) => {
     setMoleVisibility(index, false)
     setScore((prevScore) => {
-      const newScore = prevScore + 1
+      let newScore: number = prevScore;
+      switch (moles[index].moleType) {
+        case 0:
+          newScore = prevScore + 1
+          break
+        case 1:
+          newScore = prevScore - 1
+          break
+        case 2:
+          newScore = prevScore + 2
+          break
+      }
+
       if (newScore >= SUCCESS_SCORE) {
         setIsPlaying(false)
         setIsSuccess(true)
@@ -85,6 +111,21 @@ const App = () => {
     setScore(0)
     setRemainingTime(60)
     setIsSuccess(false)
+  }
+
+  const getMoleImage = (isVisible: boolean, moleType: number) => {
+    if (isVisible) {
+      switch (moleType) {
+        case 0:
+          return mole
+        case 1:
+          return blueMole
+        case 2:
+          return redMole
+      }
+    } else {
+      return hole
+    }
   }
 
   // CSS
@@ -132,14 +173,16 @@ const App = () => {
       {isSuccess ? <p css={successStyle}>成功！</p> : <p css={successStyle}></p>}
       <div css={scoreTimeStyle}>
         <span css={spanStyle}>得点: {score}</span>
-        {remainingTime <= 0 ? <span>時間切れ</span> : <span>残り時間: {remainingTime}秒</span>}
+        {remainingTime <= 0 ? <span>時間切れです。またチャレンジしてね！</span> : <span>残り時間: {remainingTime}秒</span>}
       </div>
-      {isSuccess ? <button css={buttonStyle} onClick={handleRestart}>R E S T A R T</button>
-                 : <button css={buttonStyle} onClick={handleStart}>S T A R T</button>}
+      {
+        isSuccess || remainingTime <= 0
+          ? <button css={buttonStyle} onClick={handleRestart}>R E S T A R T</button>
+          : <button css={buttonStyle} onClick={handleStart}>S T A R T</button>
+      }
       <div css={gridStyle}>
-        {moles.map((isMole, index) => (isMole ?
-          <img key={index} src={mole} onClick={() => handleClick(index)} /> :
-          <img key={index} src={hole} />
+        {moles.map((mole, index) => (
+          <img key={index} src={getMoleImage(mole.isVisible, mole.moleType)} onClick={() => mole.isVisible && handleClick(index)} />
         ))}
       </div>
     </>
